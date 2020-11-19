67bbaaa5ae49ad5b7ccea0e6aab88d22acb1b2b2
andycarmona <andres.carmona@uni-app.se>
2020-11-19 22:50:13 +0100

    Added fetch get request

--- components/ViewScreenInfo.tsx
+++ components/ViewScreenInfo.tsx
@@ -1,15 +1,77 @@
 import * as WebBrowser from "expo-web-browser";
 import React from "react";
-import { StyleSheet, Dimensions, ImageBackground } from "react-native";
+import { StyleSheet, Dimensions, ImageBackground, ActivityIndicator, ScrollView } from "react-native";
 import Colors from "../constants/Colors";
 import { MonoText } from "./StyledText";
 import { Text, View } from "./Themed";
 import WavySvg from '../components/WaveSvg';
 const image = { uri: "https://images.pexels.com/photos/89432/pexels-photo-89432.jpeg?h=350&dpr=2&auto=compress&cs=tinysrgb" };
+let id:string = "";
 
 export default class ViewScreenInfo extends React.Component {
+    state = {
+        loading: true,
+        error: false,
+        posts: [],
+    }
+
+    
+
+    componentDidMount = async () => {
+        try {
+            const response = await fetch('https://jsonplaceholder.typicode.com/posts')
+            const posts = await response.json()
+
+            this.setState({ loading: false, posts })
+        } catch (e) {
+            this.setState({ loading: false, error: true })
+        }
+    }
+
+    renderPost = ({ id, title, body } : {id :any, title:any,body:any}, i) => {
+        return (
+            <View
+                key={id}
+                style={styles.post}
+            >
+                <View style={styles.postNumber}>
+                    <Text>
+                        {i + 1}
+                    </Text>
+                </View>
+                <View style={styles.postContent}>
+                    <Text>
+                        {title}
+                    </Text>
+                    <Text style={styles.postBody}>
+                        {body}
+                    </Text>
+                </View>
+            </View>
+        )
+    }
 
     render() {
+        const { posts, loading, error } = this.state
+
+        if (loading) {
+            return (
+                <View style={styles.center}>
+                    <ActivityIndicator animating={true} />
+                </View>
+            )
+        }
+
+        if (error) {
+            return (
+                <View style={styles.center}>
+                    <Text>
+                        Failed to load posts!
+              </Text>
+                </View>
+            )
+        }
+
         return (
             <View style={styles.container}>
                 <ImageBackground source={image} style={styles.image}>
@@ -17,6 +79,9 @@ export default class ViewScreenInfo extends React.Component {
                     <View style={styles.headerContainer}>
                         <Text style={styles.headerText}>Custom Header</Text>
                     </View>
+                    <ScrollView style={styles.headerList}>
+                        {posts.map(this.renderPost)}
+                    </ScrollView>
                 </ImageBackground>
             </View>
         );
@@ -34,6 +99,10 @@ const styles = StyleSheet.create({
         marginTop: 50,
         marginHorizontal: 10
     },
+    headerList: {
+        marginTop: 150,
+        marginHorizontal: 10
+    },
     svgCurve: {
         position: 'absolute',
         width: Dimensions.get('window').width
@@ -49,5 +118,29 @@ const styles = StyleSheet.create({
         color: '#FFF',
         textAlign: 'center',
         backgroundColor: '#5000ca',
-    }
+    }, post: {
+        flexDirection: 'row',
+    },
+    postNumber: {
+        width: 50,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    postContent: {
+        flex: 1,
+        borderBottomWidth: 1,
+        borderBottomColor: '#EEE',
+        paddingVertical: 25,
+        paddingRight: 15,
+    },
+    postBody: {
+        marginTop: 10,
+        fontSize: 12,
+        color: 'lightgray',
+    },
+    center: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
 });

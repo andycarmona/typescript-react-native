cfae7473e90092b014013d2637f6056a493ad913
Andres Carmona <60385822+andres-carmona-wcar@users.noreply.github.com>
2020-11-19 11:16:42 +0100

    Added background image

--- components/ViewScreenInfo.tsx
+++ components/ViewScreenInfo.tsx
@@ -1,29 +1,34 @@
 import * as WebBrowser from "expo-web-browser";
 import React from "react";
-import { StyleSheet, Dimensions } from "react-native";
+import { StyleSheet, Dimensions, ImageBackground } from "react-native";
 import Colors from "../constants/Colors";
 import { MonoText } from "./StyledText";
 import { Text, View } from "./Themed";
 import WavySvg from '../components/WaveSvg';
+const image = { uri: "https://images.pexels.com/photos/89432/pexels-photo-89432.jpeg?h=350&dpr=2&auto=compress&cs=tinysrgb" };
+
 export default class ViewScreenInfo extends React.Component {
+
     render() {
         return (
             <View style={styles.container}>
-                <WavySvg customStyles={styles.svgCurve} />
-                <View style={styles.headerContainer}>
-                    <Text style={styles.headerText}>Custom Header</Text>
-                </View>
+                <ImageBackground source={image} style={styles.image}>
+                    <WavySvg customStyles={styles.svgCurve} />
+                    <View style={styles.headerContainer}>
+                        <Text style={styles.headerText}>Custom Header</Text>
+                    </View>
+                </ImageBackground>
             </View>
         );
     }
 }
+
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#fff',
         alignSelf: 'stretch',
-        alignItems: 'center',
     },
     headerContainer: {
         marginTop: 50,
@@ -32,6 +37,11 @@ const styles = StyleSheet.create({
     svgCurve: {
         position: 'absolute',
         width: Dimensions.get('window').width
+    },
+    image: {
+        flex: 1,
+        resizeMode: "cover",
+
     },
     headerText: {
         fontSize: 30,
